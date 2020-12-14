122b8ccbf487761a06357b82b711b3dbe215813d
Ard2238 <abhishekrdas28@gmail.com>
2020-12-15 01:18:25 +0530

    Refactored Code to use lombok library to auto generate getters ad setters

--- src/main/java/com/spring/addressbook/dto/ResponseDTO.java
+++ src/main/java/com/spring/addressbook/dto/ResponseDTO.java
@@ -1,27 +1,14 @@
 package com.spring.addressbook.dto;
 
+import lombok.Data;
+
+@Data
 public class ResponseDTO {
     private String message;
     private Object object;
 
     public ResponseDTO(String message, Object object) {
         this.message = message;
-        this.object = object;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public Object getObject() {
-        return object;
-    }
-
-    public void setObject(Object object) {
         this.object = object;
     }
 }
--- src/main/java/com/spring/addressbook/model/Contact.java
+++ src/main/java/com/spring/addressbook/model/Contact.java
@@ -1,7 +1,9 @@
 package com.spring.addressbook.model;
 
 import com.spring.addressbook.dto.ContactDTO;
+import lombok.Data;
 
+@Data
 public class Contact {
     public String fullName, address, city, state;
     int zipcode, contactId;
@@ -15,61 +17,5 @@ public class Contact {
         this.zipcode = contactDTO.zipcode;
         this.contactId = contactId;
         this.phone = contactDTO.phone;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public int getZipcode() {
-        return zipcode;
-    }
-
-    public void setZipcode(int zipcode) {
-        this.zipcode = zipcode;
-    }
-
-    public int getContactId() {
-        return contactId;
-    }
-
-    public void setContactId(int contactId) {
-        this.contactId = contactId;
-    }
-
-    public long getPhone() {
-        return phone;
-    }
-
-    public void setPhone(long phone) {
-        this.phone = phone;
     }
 }
