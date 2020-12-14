cc419f619096ad26bf6efd726357d00f413e472e
Ard2238 <abhishekrdas28@gmail.com>
2020-12-15 01:24:53 +0530

    Refcatored Code to enable logging using lombok and slf4j framework; setting application.properties to set logging levels and environment

--- src/main/java/com/spring/addressbook/AddressbookApplication.java
+++ src/main/java/com/spring/addressbook/AddressbookApplication.java
@@ -1,13 +1,17 @@
 package com.spring.addressbook;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
+@Slf4j
 public class AddressbookApplication {
     public static void main(String[] args) {
         System.out.println("Welcome to the Address Book Application");
-        SpringApplication.run(AddressbookApplication.class, args);
+        ApplicationContext context = SpringApplication.run(AddressbookApplication.class, args);
+        log.info("Employee Payroll Application started in {} environment", context.getEnvironment().getProperty("environment"));
     }
 
 }
